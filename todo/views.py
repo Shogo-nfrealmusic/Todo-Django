f100190fb8b09cbe1247bd3c0c3637557f734929
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-16 20:44:49 -0700

    Complete

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from todo.models import TodoModel
 
 # Create your views here.
@@ -12,3 +13,19 @@ class TodoDetail(DetailView):
     template_name = "detail.html"
     model = TodoModel
     
+class TodoCreate(CreateView):
+    template_name = "create.html"
+    model = TodoModel
+    fields = ('title', 'memo','priority', 'duedate')
+    success_url = reverse_lazy('list')
+
+class TodoDelete(DeleteView):
+    template_name = "delete.html"
+    model = TodoModel
+    success_url = reverse_lazy('list')
+
+class TodoUpdate(UpdateView):
+    template_name = "update.html"
+    model = TodoModel
+    fields = ('title', 'memo','priority', 'duedate')
+    success_url = reverse_lazy('list')
